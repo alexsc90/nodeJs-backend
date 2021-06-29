f310654c8027daf9cf057777f6423422b10fbceb
alexsc90 <a.s.cebreros@gmail.com>
2021-06-29 10:29:01 -0500

    middleware para manejo de errores agregado

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,9 +4,13 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-app.use(express.json());
+const { logErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
 
-moviesApi(app)
+app.use(express.json());
+moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Listening http://localhost:${config.port}`)
